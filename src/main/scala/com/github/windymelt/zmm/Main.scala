d488393d99187cf329ef1872f0fd73e7fc5983ce
windymelt <windymelt@3qe.us>
2022-10-25 02:28:53 +0900

    scalafmt

--- src/main/scala/com/github/windymelt/zmm/Main.scala
+++ src/main/scala/com/github/windymelt/zmm/Main.scala
@@ -13,27 +13,32 @@ object Main extends IOApp with VoiceVoxComponent {
     val content = IO.delay(scala.xml.XML.loadFile(filePath))
 
     IO.println("Hello Zundamon!") >>
-    IO.println("Invoking audio api...") >>
-    {
-      for {
-        x <- content
-        _ <- contentSanityCheck(x)
-        ctx <- prepareContext(x)
-        voiceVox = new ConcreteVoiceVox(ctx)
-        _ <- IO.println(ctx)
-        paths <- {
-          import cats.implicits._
-          import cats.effect.implicits._
-          val saySeq = (x \ "dialogue" \ "say").map(say => generateSay(say, voiceVox, ctx))
-          saySeq.parSequence
-        }
-        _ <- IO.println(s"Wrote to $paths")
-      } yield ()
-    } >>
-    IO.pure(ExitCode.Success)
+      IO.println("Invoking audio api...") >> {
+        for {
+          x <- content
+          _ <- contentSanityCheck(x)
+          ctx <- prepareContext(x)
+          voiceVox = new ConcreteVoiceVox(ctx)
+          _ <- IO.println(ctx)
+          paths <- {
+            import cats.implicits._
+            import cats.effect.implicits._
+            val saySeq = (x \ "dialogue" \ "say").map(say =>
+              generateSay(say, voiceVox, ctx)
+            )
+            saySeq.parSequence
+          }
+          _ <- IO.println(s"Wrote to $paths")
+        } yield ()
+      } >>
+      IO.pure(ExitCode.Success)
   }
 
-  private def generateSay(sayElem: scala.xml.Node, voiceVox: VoiceVox, ctx: Context): IO[fs2.io.file.Path] = for {
+  private def generateSay(
+      sayElem: scala.xml.Node,
+      voiceVox: VoiceVox,
+      ctx: Context
+  ): IO[fs2.io.file.Path] = for {
     aq <- buildAudioQuery(sayElem.text, sayElem \@ "by", voiceVox, ctx)
     wav <- buildWavFile(aq, sayElem \@ "by", voiceVox, ctx)
     path <- writeToFile(wav, s"voice_${sayElem.text}.wav")
@@ -43,7 +48,7 @@ object Main extends IOApp with VoiceVoxComponent {
     val checkTopElem = elem.label == "content"
     val ver = elem \@ "version" == "0.0"
 
-    if (! (checkTopElem && ver)) {
+    if (!(checkTopElem && ver)) {
       throw new Exception("Invalid scenary XML") // TODO: 丁寧なエラーメッセージ
     }
     IO.unit
@@ -70,7 +75,12 @@ object Main extends IOApp with VoiceVoxComponent {
     IO.pure(Context(voiceConfigMap, characterConfigMap))
   }
 
-  private def buildAudioQuery(text: String, character: String, voiceVox: VoiceVox, ctx: Context) = {
+  private def buildAudioQuery(
+      text: String,
+      character: String,
+      voiceVox: VoiceVox,
+      ctx: Context
+  ) = {
     val characterConfig = ctx.characterConfigMap(character)
     val voiceConfig = ctx.voiceConfigMap(characterConfig.voiceId)
     // VOICEVOX特有の実装 いずれどこかの層に分離する
@@ -78,7 +88,12 @@ object Main extends IOApp with VoiceVoxComponent {
     voiceVox.audioQuery(text, speakerId)
   }
 
-  private def buildWavFile(aq: io.circe.Json, character: String, voiceVox: VoiceVox, ctx: Context): IO[fs2.Stream[IO, Byte]] = {
+  private def buildWavFile(
+      aq: io.circe.Json,
+      character: String,
+      voiceVox: VoiceVox,
+      ctx: Context
+  ): IO[fs2.Stream[IO, Byte]] = {
     val characterConfig = ctx.characterConfigMap(character)
     val voiceConfig = ctx.voiceConfigMap(characterConfig.voiceId)
     // VOICEVOX特有の実装 いずれどこかの層に分離する
@@ -94,13 +109,14 @@ object Main extends IOApp with VoiceVoxComponent {
 }
 
 sealed trait VoiceBackendConfig
-final case class VoiceVoxBackendConfig(speakerId: String) extends VoiceBackendConfig
+final case class VoiceVoxBackendConfig(speakerId: String)
+    extends VoiceBackendConfig
 
 final case class CharacterConfig(name: String, voiceId: String)
 
 case class Context(
-  voiceConfigMap: Map[String, VoiceBackendConfig],
-  characterConfigMap: Map[String, CharacterConfig],
+    voiceConfigMap: Map[String, VoiceBackendConfig],
+    characterConfigMap: Map[String, CharacterConfig]
 )
 
 // TODO: あとでちゃんとしたCake Patternにする
@@ -127,27 +143,44 @@ trait VoiceVoxComponent {
   trait VoiceVox {
     // TODO: localhost:50021決め打ちをやめる
     type AudioQuery = Json // TODO: 必要に応じて高級なcase class / HListにする
-    def audioQuery(text: String, speaker: String): IO[AudioQuery] = client.use { c =>
-      val uri = Uri.fromString("http://localhost:50021/audio_query").map(
-        _.copy(query = org.http4s.Query.fromMap(Map("speaker" -> Seq(speaker), "text" -> Seq(text))))
-      )
-      val req = Request[IO](Method.POST, uri = uri.right.get, headers = Headers("accept" -> "application/json"))
-      c.expect[AudioQuery](req)
+    def audioQuery(text: String, speaker: String): IO[AudioQuery] = client.use {
+      c =>
+        val uri = Uri
+          .fromString("http://localhost:50021/audio_query")
+          .map(
+            _.copy(query =
+              org.http4s.Query
+                .fromMap(Map("speaker" -> Seq(speaker), "text" -> Seq(text)))
+            )
+          )
+        val req = Request[IO](
+          Method.POST,
+          uri = uri.right.get,
+          headers = Headers("accept" -> "application/json")
+        )
+        c.expect[AudioQuery](req)
     }
 
-    def synthesis(aq: AudioQuery, speaker: String): IO[fs2.Stream[IO, Byte]] = client.use { c =>
-      val uri = Uri.fromString("http://localhost:50021/synthesis").map(
-        _.copy(
-          query = org.http4s.Query.fromMap(Map("speaker" -> Seq(speaker))),
+    def synthesis(aq: AudioQuery, speaker: String): IO[fs2.Stream[IO, Byte]] =
+      client.use { c =>
+        val uri = Uri
+          .fromString("http://localhost:50021/synthesis")
+          .map(
+            _.copy(
+              query = org.http4s.Query.fromMap(Map("speaker" -> Seq(speaker)))
+            )
+          )
+        val req = Request[IO](
+          Method.POST,
+          uri = uri.right.get,
+          headers = Headers("Content-Type" -> "application/json"),
+          body = fs2.Stream.fromIterator[IO](
+            aq.toString().getBytes().toIterator,
+            64
+          ) // TODO: chinksize適当に指定しているのでなんとかする
         )
-      )
-      val req = Request[IO](Method.POST,
-        uri = uri.right.get,
-        headers = Headers("Content-Type" -> "application/json"),
-        body = fs2.Stream.fromIterator[IO](aq.toString().getBytes().toIterator, 64) // TODO: chinksize適当に指定しているのでなんとかする
-      )
-      IO.pure(c.stream(req).flatMap(_.body))
-    }
+        IO.pure(c.stream(req).flatMap(_.body))
+      }
 
     private lazy val client = EmberClientBuilder.default[IO].build
   }
