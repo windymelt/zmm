35218a397b17e2818076b7674752d6310003a3d9
Windymelt <windymelt@3qe.us>
2022-10-24 21:47:20 +0900

    こんにちはなのだ

--- src/main/scala/com/github/windymelt/zmm/Main.scala
+++ src/main/scala/com/github/windymelt/zmm/Main.scala
@@ -3,6 +3,8 @@ package com.github.windymelt.zmm
 import cats.effect.IO
 import cats.effect.IOApp
 import cats.effect.ExitCode
+import java.io.OutputStream
+import org.http4s.syntax.header
 
 object Main extends IOApp with VoiceVoxComponent {
   def run(args: List[String]) = {
@@ -14,11 +16,29 @@ object Main extends IOApp with VoiceVoxComponent {
     IO.println("Hello Zundamon!") >>
     IO.println("Invoking audio api...") >>
     buildAudioQuery >>
+    {
+      for {
+        aq <- buildAudioQuery
+        wav <- buildWavFile(aq)
+        path <- writeToFile(wav)
+        _ <- IO.println(s"Wrote to $path")
+      } yield ()
+    } >>
     IO.pure(ExitCode.Success)
   }
 
   private def buildAudioQuery = {
     voiceVox.audioQuery("こんにちはなのだ")
+  }
+
+  private def buildWavFile(aq: io.circe.Json): IO[fs2.Stream[IO, Byte]] = {
+    voiceVox.synthesis(aq)
+  }
+
+  private def writeToFile(stream: fs2.Stream[IO, Byte]) = {
+    import fs2.io.file.{Files, Path}
+    val target = Path("zundamon.wav")
+    stream.through(Files[IO].writeAll(target)).compile.drain.as(target)
   }
 }
 
@@ -46,6 +66,20 @@ trait VoiceVoxComponent {
       )
       val req = Request[IO](Method.POST, uri = uri.right.get, headers = Headers("accept" -> "application/json"))
       c.expect[AudioQuery](req)
+    }
+
+    def synthesis(aq: AudioQuery): IO[fs2.Stream[IO, Byte]] = client.use { c =>
+      val uri = Uri.fromString("http://localhost:50021/synthesis").map(
+        _.copy(
+          query = org.http4s.Query.fromMap(Map("speaker" -> Seq("1"))),
+        )
+      )
+      val req = Request[IO](Method.POST,
+        uri = uri.right.get,
+        headers = Headers("Content-Type" -> "application/json"),
+        body = fs2.Stream.fromIterator[IO](aq.toString().getBytes().toIterator, 64) // TODO: chinksize適当に指定しているのでなんとかする
+      )
+      IO.pure(c.stream(req).flatMap(_.body))
     }
 
     private def client = EmberClientBuilder.default[IO].build
