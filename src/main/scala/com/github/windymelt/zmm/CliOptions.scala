7fe0ec0de01790a868d37a92b0aa0d80e547413a
windymelt <windymelt@3qe.us>
2023-05-01 03:28:54 +0900

    blank lines and comment

--- src/main/scala/com/github/windymelt/zmm/CliOptions.scala
+++ src/main/scala/com/github/windymelt/zmm/CliOptions.scala
@@ -6,15 +6,37 @@ import cats.implicits._
 import com.monovore.decline._
 import com.monovore.decline.effect._
 
+/** ZMMの動作モードを決定するオプション。これらのうちいずれかを選択してZMMは起動する
+  */
 sealed trait ZmmOption
+
+/** 設定等を表示するためのモード。
+  *
+  * @param target
+  */
 final case class ShowCommand(target: String)
     extends ZmmOption // 今のところvoicevoxしか入らない
+/** 動画生成を行うモード。
+  *
+  * @param targetFile
+  *   原稿XMLファイルへのパス
+  * @param outputFile
+  *   出力先MP4ファイルへのパス
+  * @param screenShotBackend
+  *   スクリーンショットのバックエンドに何を使うか
+  */
 final case class Generate(
     targetFile: TargetFile,
     outputFile: java.nio.file.Path,
     screenShotBackend: Option[ScreenShotBackend]
 ) extends ZmmOption
+
+/** ディレクトリをZMMのプロジェクトとして初期化するモード。
+  */
 final case class InitializeCommand() extends ZmmOption
+
+/** バージョンを表示させる。
+  */
 final case class VersionFlag() extends ZmmOption
 
 final case class TargetFile(target: java.nio.file.Path)
@@ -30,10 +52,12 @@ object CliOptions {
     Opts.subcommand(name = "show", help = "Prints information.")(
       Opts.argument[String]("voicevox").map(ShowCommand.apply)
     )
+
   private val targetFile =
     Opts
       .argument[java.nio.file.Path](metavar = "XMLFile")
       .map(TargetFile.apply)
+
   private val outputFile =
     Opts
       .option[java.nio.file.Path](
@@ -43,6 +67,7 @@ object CliOptions {
         metavar = "OUTPUT.mp4"
       )
       .withDefault(java.nio.file.Path.of("output_with_bgm.mp4"))
+
   private val screenShotBackend = Opts
     .option[String](
       "screenshot",
@@ -61,6 +86,7 @@ object CliOptions {
         )
     }
     .orNone
+
   private val generate =
     (targetFile, outputFile, screenShotBackend) mapN (Generate.apply)
 
@@ -68,6 +94,7 @@ object CliOptions {
     name = "init",
     help = "Initializes current directory as ZMM project."
   )(Opts.unit.map(_ => InitializeCommand()))
+
   private val versionOption = Opts
     .flag("version", help = "Show version", short = "v")
     .map(_ => VersionFlag())
