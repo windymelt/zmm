fe5438c66547456885b3f97ed55df81f0fab9662
windymelt <windymelt@3qe.us>
2024-02-05 02:44:42 +0900

    introduce alias path

---
+++ src/test/scala/com/github/windymelt/zmm/util/PathAliasSpec.scala
@@ -0,0 +1,21 @@
+package com.github.windymelt.zmm.util
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class PathAliasSpec extends AnyFlatSpec with Matchers {
+
+  "PathAlias" should "resolve @assets" in {
+    PathAlias.resolve("./do/not/modify", "template") shouldBe "./do/not/modify"
+
+    PathAlias.resolve(
+      "@assets/foo/bar.png",
+      "template"
+    ) shouldBe "../../assets/foo/bar.png"
+
+    PathAlias.resolve(
+      "@assets/foo/bar.mp3",
+      "ffmpeg"
+    ) shouldBe "./assets/foo/bar.mp3"
+  }
+}
